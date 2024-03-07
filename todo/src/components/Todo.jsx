f14862e36cf4afc596cc7d70c8664ddc8d5fbe50
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-03-07 23:36:57 +0530

    made the todo from chakra UI and tailwind css in which user can create, edit and delete a todo

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -5,20 +5,46 @@ import {
   Input,
   ListIcon,
   ListItem,
+  Tooltip,
   UnorderedList,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { FaRegEdit } from "react-icons/fa";
+import { MdOutlineDelete } from "react-icons/md";
 
 const Todo = () => {
   const [todoText, setTodoText] = useState("");
-  const [todoList, setTodoList] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
 
-  //function for creating todo
+  // Function for creating or updating todo
   const createTodo = () => {
-    setTodoList((todo) => [todoText, ...todo]);
+    if (editIndex === -1) {
+      setTodoList((todos) => [...todos, todoText, ]);
+    } else {
+      const updatedTodos = [...todoList];
+      updatedTodos[editIndex] = todoText;
+      setTodoList(updatedTodos);
+      setEditIndex(-1);
+    }
     setTodoText("");
   };
-  
+
+  const editText = (text, index) => {
+    setEditIndex(index);
+    setTodoText(text);
+  };
+
+  //function for deleting todo
+  const deleteTodo = (todo, index) => {
+    // const updatedList = [...todoList]
+    // updatedList.splice(index, 1)
+    // setTodoList(updatedList)
+
+    const updatedTodo = todoList.filter((_, i ) => i !== index)
+    setTodoList(updatedTodo)
+  }
+
   return (
     <Container className="flex flex-col mt-10 gap-5">
       <Box className="flex gap-5">
@@ -27,14 +53,41 @@ const Todo = () => {
           onChange={(e) => setTodoText(e.target.value)}
           value={todoText}
         />
-        <Button onClick={createTodo}>Create</Button>
+        <Button onClick={createTodo}>
+          {editIndex === -1 ? "Create" : "Update"}
+        </Button>
+        {editIndex !== -1 && <Button>Cancel</Button>}
       </Box>
       <Box>
         <UnorderedList>
-          {todoList.length > 0 ?(
-          todoList.map((todo) => (
-            <ListItem>{todo} <ListIcon></ListIcon></ListItem>
-          ))) : (<></>)}
+          {todoList.length > 0 ? (
+            todoList.map((todo, index) => (
+              <ListItem
+                className="flex gap-5 items-center"
+                key={index}
+              >
+                {todo}
+                <Box>
+                <Tooltip label="Edit">
+                  <ListIcon
+                    onClick={() => editText(todo, index)}
+                    cursor="pointer"
+                  >
+                    <FaRegEdit className="text-2xl" />
+                  </ListIcon>
+                </Tooltip>
+
+                <Tooltip label="Delete">
+                  <ListIcon cursor="pointer" onClick={() => deleteTodo(todo, index)}>
+                    <MdOutlineDelete className="text-2xl"/>
+                  </ListIcon>
+                </Tooltip>
+                </Box>
+              </ListItem>
+            ))
+          ) : (
+            <></>
+          )}
         </UnorderedList>
       </Box>
     </Container>
