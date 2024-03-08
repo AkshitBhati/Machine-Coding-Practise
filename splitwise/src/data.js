11f6b3e3e25ab37c2d77238776094c7d2c48dea9
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-03-08 12:17:03 +0530

    completing home page of splitwise

---
+++ splitwise/src/data.js
@@ -0,0 +1,28 @@
+export const users = [
+    {
+        id:1,
+        name:"user1",
+        email:"user1@gmail.com",
+        mobilenumber:"+912345678900"
+    },
+    {
+        id:2,
+        name:"user2",
+        email:"user2@gmail.com",
+        mobilenumber:"+912345678900"
+    },
+    {
+        id:3,
+        name:"user3",
+        email:"user3@gmail.com",
+        mobilenumber:"+912345678900"
+    },
+    {
+        id:4,
+        name:"user4",
+        email:"user4@gmail.com",
+        mobilenumber:"+912345678900"
+    }
+]
+
+ 
---
+++ splitwise/src/page/Home.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  AlertDialogCloseButton,
+  Button,
+  Box,
+  Input,
+  useDisclosure,
+  VStack,
+  Container
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+
+const Home = () => {
+  const [nameOfGroup, setNameOfGroup] = useState("");
+  const [groups, setGroups] = useState([]);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+
+  const createGroup = () => {
+    try {
+      const data = {
+        id: uuid(),
+        nameOfGroup: nameOfGroup,
+      };
+      localStorage.setItem("groups", JSON.stringify([...groups, data]));
+      setGroups((prevGroups) => [...prevGroups, data]);
+      onClose();
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    const data = localStorage.getItem("groups");
+    const parsedData = JSON.parse(data);
+    setGroups(parsedData || []);
+  }, []);
+
+  return (
+    <>
+      <VStack className="">
+      <Container maxW='5xl' className="flex flex-col gap-2 mt-10">
+        {groups.length > 0 ? (groups.map((data) => (
+          <Box key={data.id} className=" bg-blue-500 p-2 text-white rounded-md ">
+            {data.nameOfGroup}
+          </Box>
+        ))) : (<p>No group exists</p>)}
+        <Box className="mt-5 ">
+          <Button className="" onClick={onOpen}>
+            Split Bills
+          </Button>
+        </Box>
+      </Container>
+      </VStack>
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+        <AlertDialogContent>
+          <AlertDialogHeader>Create group</AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>
+            <Input
+              type="text"
+              placeholder="Name of the group"
+              onChange={(e) => setNameOfGroup(e.target.value)}
+            />
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="red" ml={3} onClick={createGroup}>
+              Create
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+};
+
+export default Home;
